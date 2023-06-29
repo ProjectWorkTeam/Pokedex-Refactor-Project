21efcbf245824ec9e48e037900afa4bbebb93b7d
alex5 <alex@kimnetics.com>
2023-06-29 05:50:12 -1000

    basic directories added

--- pokedex-refactor- flask/flask-backend/app/__init__.py
+++ pokedex-refactor- flask/flask-backend/app/__init__.py
@@ -1,8 +1,26 @@
 
 # import statement for CSRF
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
+def create_app():
+    app = Flask(__name__)
+
+    # Configure database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE NAME HERE'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    #blueprints here?
+
+    return app
 
 # after request code for CSRF token injection
 @app.after_request
